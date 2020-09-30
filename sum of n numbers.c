dd7445dfdc62da4a5ce287e98108fe7ee1adaa8e
priyanshkulshrestha <65914664+priyanshkulshrestha@users.noreply.github.com>
2020-10-01 00:47:33 +0530

    sum of n number in O(1) time compexity
    In this program I used formula instead of using for loop this will make this program more efficient and fast because in for loop you have to use 1 more variable i which increase space complexity and if you use for loop it will increase time complexity and for loop do this task by running one line n times so it will increase time complexity.
    so you may change your program and make it more efficient ,fast and less complex.

--- sum of n numbers.c
+++ sum of n numbers.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 int main() {
-    int n, i, sum = 0;
+    int n, sum;
 
     printf("Enter a positive integer: ");
     scanf("%d", &n);
-
-    for (i = 1; i <= n; ++i) {
-        sum += i;
-    }
-
+    
+    sum = (n*n+n)/2;
+    
     printf("Sum = %d", sum);
     return 0;
 }
